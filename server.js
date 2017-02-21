5f4d46e6178168766cf5e1f969fedd070afea230
Michael Snaddon <msnaddon0991@gmail.com>
2017-02-21 17:13:45 +0000

    minor patch to mock data

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ var app = express();
 var path = require('path');
 var data = require('./data/mockData.json')
 
-//overwriting data for test purposes
-
-const today = new Date();
-const tomorrow = new Date(today.getTime() + 93600000);
-const dayAfter = new Date(tomorrow.getTime() + 82800000);
-
+// Overwriting data for test purposes with dynamic dating.
+const today = new Date().getTime();
+const tomorrow = today + 93600000;
+const dayAfter = tomorrow + 82800000;
 data = {
 	"events": [
 	{
 		"title": "Dog walking, advanced class",
-		"date": today.toString(),
+		"date": today,
 		"description": [
 			"Dogs will walk you for 1 hour whilst you bring them tennis balls",
 			"Come join the whimsical sniffs and smells of Princes Gardens while our dogs idly slump by playing Pokemon Go.",
@@ -22,7 +20,7 @@ data = {
 	},
 	{
 		"title": "Codebase Weekly Yoga",
-		"date": today.toString(),
+		"date": today,
 		"description": [
 			"Bring your mat and some water for some stretching, de-stressing and exercise. For information, see Reception.",
 			"£8 drop in £45 for six sessions. 1PM, Bruntsfield Links",
@@ -33,13 +31,13 @@ data = {
 	},
 	{
 		"title": "Weekly Fight Club at the underground parking lot (East-End)",
-		"date": tomorrow.toString(),
+		"date": tomorrow,
 		"description": ["Noone talks about it", "No, seriously, we don't", "Stop asking."],
 		"hosts": ["Fight Club inc."]
 	},
 	{
 		"title": "Scottish blockchain meetup",
-		"date": dayAfter.toString(),
+		"date": dayAfter,
 		"description": ["We're still in the early days of what could prove to be one of the most important innovations since the early days of the internet. The group is open to everyone involved in the blockchain and Bitcoin communities and those that aren't. Experts, amateurs, miners, critics, whoever. If you're interested, you're welcome.",
 			"We're an inclusive bunch - come along and find out what's going on in the Scottish scene from those that are at the heart of the activity."],
 		"hosts": ["Greig Paul"]
